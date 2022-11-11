5044436c391d03ef54a58dec0a41f238798e392c
dhirajnikam <dhirajnikam78@gmail.com>
2022-11-11 16:37:03 +0530

    funRow Listview with two constant

--- lib/src/foundation/mathFiels.dart
+++ lib/src/foundation/mathFiels.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:atomic_latex/src/foundation/keyboardField.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Func extends StatelessWidget {
   var aplpha;
@@ -38,84 +39,111 @@ class Func extends StatelessWidget {
   var betaGammaC;
 
   var delimiterC;
-  Func({
-    Key? key,
-    required this.aplpha,
-    required this.x2,
-    required this.sin,
-    required this.fx,
-    required this.elnx,
-    required this.intdx,
-    required this.matrix,
-    required this.betaGamma,
-    required this.delimiter,
-    required this.aplphaC,
-    required this.x2C,
-    required this.sinC,
-    required this.fxC,
-    required this.elnxC,
-    required this.intdxC,
-    required this.matrixC,
-    required this.betaGammaC,
-    required this.delimiterC,
-  }) : super(key: key);
+
+  var nextTab;
+
+  var previousTab;
+  Func(
+      {Key? key,
+      required this.aplpha,
+      required this.x2,
+      required this.sin,
+      required this.fx,
+      required this.elnx,
+      required this.intdx,
+      required this.matrix,
+      required this.betaGamma,
+      required this.delimiter,
+      required this.aplphaC,
+      required this.x2C,
+      required this.sinC,
+      required this.fxC,
+      required this.elnxC,
+      required this.intdxC,
+      required this.matrixC,
+      required this.betaGammaC,
+      required this.delimiterC,
+      required this.nextTab,
+      required this.previousTab})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        FunctionButton(
-          onTab: aplpha,
-          KeyboardText: "abc",
-          fontSize: 12,
-          color: aplphaC,
+        ButtonFuc(
+          tap: previousTab,
+          color: Colors.amber,
+          icon: Icons.skip_previous,
+          iconsColors: Colors.blue,
         ),
-        FunctionButton(
-          onTab: x2,
-          KeyboardText: r"$x^2$",
-          fontSize: 20,
-          color: x2C,
+        Expanded(
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            children: [
+              FunctionButton(
+                onTab: aplpha,
+                KeyboardText: "abc",
+                fontSize: 12,
+                color: aplphaC,
+              ),
+              FunctionButton(
+                onTab: x2,
+                KeyboardText: r"$x^2$",
+                fontSize: 12,
+                color: x2C,
+              ),
+              FunctionButton(
+                fontSize: 12,
+                onTab: sin,
+                KeyboardText: r"$sin{x}$",
+                color: sinC,
+              ),
+              FunctionButton(
+                onTab: fx,
+                KeyboardText: r"$f(x)$",
+                fontSize: 12,
+                color: fxC,
+              ),
+              FunctionButton(
+                onTab: elnx,
+                KeyboardText: r"$e^{\ln(x)}$",
+                fontSize: 12,
+                color: elnxC,
+              ),
+              FunctionButton(
+                onTab: intdx,
+                KeyboardText: r"$\int{dx}$",
+                fontSize: 10,
+                color: intdxC,
+              ),
+              FunctionButton(
+                onTab: matrix,
+                KeyboardText: r"$\begin{bmatrix}  x\\y  \end{bmatrix}$",
+                fontSize: 10,
+                color: matrixC,
+              ),
+              FunctionButton(
+                onTab: betaGamma,
+                KeyboardText: r"$\alpha\beta\gamma$",
+                fontSize: 12,
+                color: betaGammaC,
+              ),
+              // FunctionButton(
+              //   onTab: delimiter,
+              //   KeyboardText: r"$",
+              //   fontSize: 20,
+              //   color: delimiterC,
+              // ),
+            ],
+          ),
         ),
-        FunctionButton(
-          onTab: sin,
-          KeyboardText: r"$sin{x}$",
-          color: sinC,
-        ),
-        FunctionButton(
-          onTab: fx,
-          KeyboardText: r"$f(x)$",
-          fontSize: 12,
-          color: fxC,
-        ),
-        FunctionButton(
-          onTab: elnx,
-          KeyboardText: r"$e^{\ln(x)}$",
-          fontSize: 12,
-          color: elnxC,
-        ),
-        FunctionButton(
-          onTab: intdx,
-          KeyboardText: r"$\int{dx}$",
-          fontSize: 12,
-          color: intdxC,
-        ),
-        FunctionButton(
-          onTab: matrix,
-          KeyboardText: r"$\begin{bmatrix}  x\\y  \end{bmatrix}$",
-          fontSize: 12,
-          color: matrixC,
-        ),
-        FunctionButton(
-          onTab: betaGamma,
-          KeyboardText: r"$\alpha\beta\gamma$",
-          fontSize: 12,
-          color: betaGammaC,
-        ),
-        FunctionButton(
-          onTab: delimiter,
-          KeyboardText: r"$",
-          fontSize: 20,
-          color: delimiterC,
+        ButtonFuc(
+          tap: nextTab,
+          color: Colors.amber,
+          icon: Icons.skip_next,
+          iconsColors: Colors.blue,
         ),
       ],
     );
